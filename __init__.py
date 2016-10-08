b8215145b3733772d325479dd9d96dc60ef97311
Alexey-t <support@uvviewsoft.com>
2016-10-08 21:41:08 +0300

    refac

--- __init__.py
+++ __init__.py
@@ -11,13 +11,10 @@ def is_wordchar(s):
 
 class Command:
     def on_complete(self, ed_self):
-        fn = ed.get_filename()
-        carets = ed.get_carets()
-        if len(carets)!=1: return True
-        x0, y0, x1, y1 = carets[0]
-
-        if not 0 <= y0 < ed.get_line_count(): 
-            return True
+        params = self.get_params()
+        if not params: return
+    
+        text, fn, y0, x0 = params
         line = ed.get_text_line(y0)
         if not 0 < x0 <= len(line): 
             return True        
@@ -36,11 +33,9 @@ class Command:
         #print('len2', len2)
         if len1<=0: return True
         
-        text = ed.get_text_all()
+        text = handle_autocomplete(*params)
         if not text: return True
         
-        text = handle_autocomplete(text, fn, y0, x0)
-        if not text: return True
         ed.complete(text, len1, len2)
         return True
         
