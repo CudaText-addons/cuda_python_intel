139e2efe5b7686a339117d95a75750d834b4c9d2
Alexey-t <support@uvviewsoft.com>
2017-02-14 01:24:34 +0300

    autocomp after ed.

--- __init__.py
+++ __init__.py
@@ -7,22 +7,24 @@ LINE_GOTO_OFFSET = 5
 
 def is_wordchar(s):
     return (s=='_') or s.isalnum()
-    
+
 
 class Command:
     def on_complete(self, ed_self):
         params = self.get_params()
         if not params: return
-    
+
         text, fn, y0, x0 = params
         line = ed.get_text_line(y0)
-        if not 0 < x0 <= len(line): 
-            return True        
-        
+        if not 0 < x0 <= len(line):
+            return True
+
         #calc len left
         x = x0
         while x>0 and is_wordchar(line[x-1]): x -= 1
         len1 = x0-x
+
+        after_dot = x>1 and line[x-1]=='.'
 
         #calc len right
         x = x0
@@ -31,23 +33,24 @@ class Command:
 
         #print('len1', len1)
         #print('len2', len2)
-        if len1<=0: return True
-        
+        if len1<=0 and not after_dot:
+            return True
+
         text = handle_autocomplete(*params)
         if not text: return True
-        
+
         ed.complete(text, len1, len2)
         return True
-        
-        
+
+
     def on_goto_def(self, ed_self):
         params = self.get_params()
         if not params: return True
-        
+
         res = handle_goto_def(*params)
         if res is None: return True
 
-        #res has (filename, y, x)        
+        #res has (filename, y, x)
         self.goto_file(*res)
         return True
 
@@ -55,18 +58,18 @@ class Command:
     def on_func_hint(self, ed_self):
         params = self.get_params()
         if not params: return
-        
+
         item = handle_func_hint(*params)
         if item is None:
             return
         else:
             return ' '+item
-            
+
 
     def show_docstring(self):
         params = self.get_params()
         if not params: return
-        
+
         text = handle_docstring(*params)
         if text:
             app_log(LOG_SET_PANEL, LOG_PANEL_OUTPUT)
@@ -83,12 +86,12 @@ class Command:
     def show_usages(self):
         params = self.get_params()
         if not params: return
-        
+
         items = handle_usages(*params)
         if not items:
             msg_status('Cannot find usages')
             return
-            
+
         items_show = [
             os.path.basename(item[0])+
             ', Line %s, Col %d' %(item[1]+1, item[2]+1)+
@@ -97,18 +100,18 @@ class Command:
             ]
         res = dlg_menu(MENU_LIST_ALT, '\n'.join(items_show))
         if res is None: return
-        
+
         item = items[res]
-        self.goto_file(item[0], item[1], item[2]) 
-        
-        
+        self.goto_file(item[0], item[1], item[2])
+
+
     def goto_file(self, filename, num_line, num_col):
         if not os.path.isfile(filename): return
-        
+
         file_open(filename)
         ed.set_prop(PROP_LINE_TOP, str(max(0, num_line-LINE_GOTO_OFFSET)))
         ed.set_caret(num_col, num_line)
-        
+
         msg_status('Go to file: '+filename)
         print('Goto "%s", Line %d'%(filename, num_line+1))
 
@@ -119,13 +122,13 @@ class Command:
         if len(carets)!=1: return
         x0, y0, x1, y1 = carets[0]
 
-        if not 0 <= y0 < ed.get_line_count(): 
+        if not 0 <= y0 < ed.get_line_count():
             return
         line = ed.get_text_line(y0)
-        if not 0 <= x0 <= len(line): 
+        if not 0 <= x0 <= len(line):
             return
-            
+
         text = ed.get_text_all()
         if not text: return
-        
+
         return (text, fn, y0, x0)
